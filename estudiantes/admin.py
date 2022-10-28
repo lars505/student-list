5dfc072e6a24237e8e53430686630bed13bcf89c
Angel Lars <lars50593@gmail.com>
2022-10-27 23:37:09 -0600

    regstro del modelo

--- estudiantes/admin.py
+++ estudiantes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Estudiante
+
 # Register your models here.
+
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = ["correo","nombre","apellidos","edad"]
+
+admin.site.register(Estudiante, EstudianteAdmin)
